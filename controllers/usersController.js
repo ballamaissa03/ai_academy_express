91fe4a04f9a3877ec2f7d0dde45592cf2a6c4b0a
ballamaissa03 <astoudiouf@esp.sn>
2025-04-29 11:06:49 +0100

    LAB9

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,5 @@
+const User = require("../models/User");
+
 // Fonction utilitaire pour extraire les paramètres utilisateur du corps de la requête
 const getUserParams = body => {
     return {
@@ -10,96 +12,110 @@ const getUserParams = body => {
         zipCode: body.zipCode
     };
 };
-    module.exports = {
-        index: (req, res, next) => {
-            User.find({})
-            .then(users => {
-                res.locals.users = users;
-                next();
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la récupération des utilisateurs: ${error.message}`);
-                next(error);
+
+module.exports = {
+    // Méthode pour récupérer les utilisateurs et les passer à la vue
+    index: (req, res, next) => {
+        User.find({})
+        .then(users => {
+            res.locals.users = users;
+            res.locals.pageTitle = 'Liste des utilisateurs'; // Définir la variable pageTitle
+            next();
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la récupération des utilisateurs: ${error.message}`);
+            next(error);
+        });
+    },
+
+    // Méthode pour afficher la vue de la liste des utilisateurs
+    indexView: (req, res) => {
+        res.render("users/index", { pageTitle: res.locals.pageTitle, users: res.locals.users }); // Passer pageTitle et users à la vue
+    },
+
+    new: (req, res) => {
+        res.render("users/new");
+    },
+
+    create: (req, res, next) => {
+        let userParams = getUserParams(req.body);
+        User.create(userParams)
+        .then(user => {
+            res.locals.redirect = "/users";
+            res.locals.user = user;
+            next();
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la création de l'utilisateur: ${error.message}`);
+            res.locals.redirect = "/users/new";
+            next();
+        });
+    },
+
+    redirectView: (req, res, next) => {
+        let redirectPath = res.locals.redirect;
+        if (redirectPath) res.redirect(redirectPath);
+        else next();
+    },
+
+    show: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+        .then(user => {
+            res.locals.user = user;
+            next();
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
+            next(error);
+        });
+    },
+
+    showView: (req, res) => {
+        res.render("users/show");
+    },
+
+    edit: (req, res, next) => {
+        let userId = req.params.id;
+        User.findById(userId)
+        .then(user => {
+            res.render("users/edit", {
+                user: user
             });
-        },
-        indexView: (req, res) => {
-            res.render("users/index");
-        },
-        new: (req, res) => {
-            res.render("users/new");
-        },
-        create: (req, res, next) => {
-            let userParams = getUserParams(req.body);
-            User.create(userParams)
-            .then(user => {
-                res.locals.redirect = "/users";
-                res.locals.user = user;
-                next();
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la création de l'utilisateur: ${error.message}`);
-                res.locals.redirect = "/users/new";
-                next();
-            });
-        },
-        redirectView: (req, res, next) => {
-            let redirectPath = res.locals.redirect;
-            if (redirectPath) res.redirect(redirectPath);
-            else next();
-        },
-        show: (req, res, next) => {
-            let userId = req.params.id;
-            User.findById(userId)
-            .then(user => {
-                res.locals.user = user;
-                next();
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
-                next(error);
-            });
-        },
-        showView: (req, res) => {
-            res.render("users/show");
-        },
-        edit: (req, res, next) => {
-            let userId = req.params.id;
-            User.findById(userId)
-            .then(user => {
-                res.render("users/edit", {
-                    user: user
-                });
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
-                next(error);
-            });
-        },
-        update: (req, res, next) => {
-            let userId = req.params.id,
-            userParams = getUserParams(req.body);User.findByIdAndUpdate(userId, {
-                $set: userParams
-            })
-            .then(user => {
-                res.locals.redirect = `/users/${userId}`;
-                res.locals.user = user;
-                next();
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la mise à jour de l'utilisateur par ID: ${error.message}`);
-                next(error);
-            });
-        },
-        delete: (req, res, next) => {
-            let userId = req.params.id;
-            User.findByIdAndRemove(userId)
-            .then(() => {
-                res.locals.redirect = "/users";
-                next();
-            })
-            .catch(error => {
-                console.log(`Erreur lors de la suppression de l'utilisateur par ID: ${error.message}`);
-                next();
-            });
-        }
-    };
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la récupération de l'utilisateur par ID: ${error.message}`);
+            next(error);
+        });
+    },
+
+    update: (req, res, next) => {
+        let userId = req.params.id,
+        userParams = getUserParams(req.body);
+        User.findByIdAndUpdate(userId, {
+            $set: userParams
+        })
+        .then(user => {
+            res.locals.redirect = `/users/${userId}`;
+            res.locals.user = user;
+            next();
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la mise à jour de l'utilisateur par ID: ${error.message}`);
+            next(error);
+        });
+    },
+
+    delete: (req, res, next) => {
+        let userId = req.params.id;
+        User.findByIdAndRemove(userId)
+        .then(() => {
+            res.locals.redirect = "/users";
+            next();
+        })
+        .catch(error => {
+            console.log(`Erreur lors de la suppression de l'utilisateur par ID: ${error.message}`);
+            next();
+        });
+    }
+};
