27896d7d0404df1a5b4fcc3e4e15ea306075fc37
ballamaissa03 <astoudiouf@esp.sn>
2025-04-29 11:29:35 +0100

    REPs

--- main.js
+++ main.js
@@ -1,67 +1,90 @@
 const express = require("express");
 const layouts = require("express-ejs-layouts");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+const flash = require("connect-flash");
+const passport = require("passport");
+const authController = require("./controllers/authController");
 const homeController = require("./controllers/homeController");
 const errorController = require("./controllers/errorController");
 const subscribersController = require("./controllers/subscribersController");
-const mongoose = require("mongoose"); // Ajout de Mongoose
 const usersController = require("./controllers/usersController");
 const coursesController = require("./controllers/coursesController");
-
 // Configuration de la connexion à MongoDB
-mongoose.connect("mongodb://localhost:27017/ai_academy");
+mongoose.connect(
+"mongodb://localhost:27017/ai_academy",
+{ useNewUrlParser: true }
+);
 const db = mongoose.connection;
 db.once("open", () => {
-  console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
+console.log("Connexion réussie à MongoDB en utilisant Mongoose!");
 });
 const app = express();
-// Définir le port
+// Configuration de l'application
 app.set("port", process.env.PORT || 3000);
-// Configuration d'EJS comme moteur de template
 app.set("view engine", "ejs");
-app.use(layouts);
-// Middleware pour traiter les données des formulaires
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
-
-app.use(express.json());
-// Servir les fichiers statiques
 app.use(express.static("public"));
-// Middleware pour les messages
-app.use((req, res, next) => {
-  res.locals.messages = {
-    success: null,
-    error: null,
-    info: null,
-  };
-  next();
-});
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(err.status || 500).send('Internal Server Error');
-});
-
-const methodOverride = require("method-override");
+app.use(layouts);
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(methodOverride("_method", {
 methods: ["POST", "GET"]
 }));
-// Définir les routes
+// Configuration des cookies et des sessions
+app.use(cookieParser("secret_passcode"));
+app.use(session({
+secret: "secret_passcode",
+cookie: {
+maxAge: 4000000
+},
+resave: false,
+saveUninitialized: false
+}));
+// Configuration de flash messages
+app.use(flash());
+// Configuration de Passport
+app.use(passport.initialize());
+app.use(passport.session());
+// Configuration du User model pour Passport
+const User = require("./models/User");
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+// Middleware pour rendre les variables locales disponibles dans toutes les vues
+app.use((req, res, next) => {
+res.locals.flashMessages = req.flash();
+res.locals.loggedIn = req.isAuthenticated();
+res.locals.currentUser = req.user;
+next();
+});
+// Routes...
+
 app.get("/", homeController.index);
 app.get("/about", homeController.about);
 app.get("/courses", homeController.courses);
-app.get("/faq", homeController.faq);
 app.get("/contact", homeController.contact);
 app.post("/contact", homeController.processContact);
+
+// Routes abonnés
 app.get("/subscribers", subscribersController.getAllSubscribers);
 app.get("/subscribers/new", subscribersController.getSubscriptionPage);
 app.post("/subscribers/create", subscribersController.saveSubscriber);
-app.get("/subscribers/search", subscribersController.searchSubscribers);
 app.get("/subscribers/:id", subscribersController.show);
-app.post("/subscribers/:id/delete", subscribersController.deleteSubscriber);
-app.get("/subscribers/:id/edit", subscribersController.editSubscriber);
-app.post("/subscribers/:id/update", subscribersController.updateSubscriber);
+
+// Routes d'authentification
+app.get("/login", authController.login);
+app.post("/login", authController.authenticate);
+app.get("/logout", authController.logout, usersController.redirectView);
+app.get("/signup", authController.signup);
+app.post("/signup", authController.register, usersController.redirectView);
+// Routes protégées - accessibles uniquement aux utilisateurs connectés
+app.use("/users", authController.ensureLoggedIn);
+app.use("/courses/new", authController.ensureLoggedIn);
+app.use("/courses/:id/edit", authController.ensureLoggedIn);
+
+
 // Routes pour les utilisateurs
 app.get("/users", usersController.index, usersController.indexView);
 app.get("/users/new", usersController.new);
@@ -70,13 +93,6 @@ app.get("/users/:id", usersController.show, usersController.showView);
 app.get("/users/:id/edit", usersController.edit);
 app.put("/users/:id/update", usersController.update, usersController.redirectView);
 app.delete("/users/:id/delete", usersController.delete, usersController.redirectView);
-app.post('/users', (req, res, next) => {
-  const newUser = new User(req.body);
-  newUser.save()
-    .then(user => res.status(201).json(user))
-    .catch(next); // Assurez-vous que les erreurs sont correctement transmises au middleware de gestion des erreurs
-});
-
 // Routes pour les cours
 app.get("/courses", coursesController.index, coursesController.indexView);
 app.get("/courses/new", coursesController.new);
@@ -86,13 +102,9 @@ app.get("/courses/:id/edit", coursesController.edit);
 app.put("/courses/:id/update", coursesController.update, coursesController.redirectView);
 app.delete("/courses/:id/delete", coursesController.delete, coursesController.redirectView);
 
-// Gestion des erreurs
 app.use(errorController.pageNotFoundError);
 app.use(errorController.internalServerError);
-// Démarrer le serveur
+
 app.listen(app.get("port"), () => {
-  console.log(`Le serveur a démarré et écoute sur le port: ${app.get("port")}`);
-  console.log(
-    `Serveur accessible à l'adresse: http://localhost:${app.get("port")}`
-  );
+  console.log(`Serveur démarré sur http://localhost:${app.get("port")}`);
 });
--- models/user.js
+++ models/user.js
@@ -1,57 +1,66 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const passportLocalMongoose = require("passport-local-mongoose");
+
 const userSchema = new Schema(
-{
-name: {
-first: {
-type: String,
-trim: true
-},
-last: {
-type: String,
-trim: true
-}
-},
-email: {
-type: String,
-required: true,
-lowercase: true,
-unique: true
-},
-zipCode: {
-type: Number,
-min: [10000, "Code postal trop court"],
-max: 99999
-},
-password: {
-type: String,required: true
-},
-courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
-subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" }
-},
-{
-timestamps: true
-}
+  {
+    name: {
+      first: {
+        type: String,
+        trim: true
+      },
+      last: {
+        type: String,
+        trim: true
+      }
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true
+    },
+    zipCode: {
+      type: Number,
+      min: [10000, "Code postal trop court"],
+      max: 99999
+    },
+    courses: [{ type: Schema.Types.ObjectId, ref: "Course" }],
+    subscribedAccount: { type: Schema.Types.ObjectId, ref: "Subscriber" }
+  },
+  {
+    timestamps: true
+  }
 );
+
+// Ajouter le plugin passport-local-mongoose
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: "email"
+});
+
 // Attribut virtuel pour le nom complet
-userSchema.virtual("fullName").get(function() {
-return `${this.name.first} ${this.name.last}`;
+userSchema.virtual("fullName").get(function () {
+  return `${this.name.first} ${this.name.last}`;
 });
-// Hook pre-save pour associer un abonné à l'utilisateur si les emails correspondent
-userSchema.pre("save", function(next) {
-let user = this;
-if (user.subscribedAccount === undefined) {
-mongoose.model("Subscriber").findOne({ email: user.email })
-.then(subscriber => {
-user.subscribedAccount = subscriber;
-next();
-})
-.catch(error => {
-console.log(`Erreur lors de la connexion avec l'abonné: ${error.message}`);
-next(error);
+
+// Hook pre-save pour associer un abonné à l'utilisateur
+userSchema.pre("save", function (next) {
+  let user = this;
+  if (user.subscribedAccount === undefined) {
+    mongoose
+      .model("Subscriber")
+      .findOne({ email: user.email })
+      .then((subscriber) => {
+        user.subscribedAccount = subscriber;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Erreur lors de la connexion avec l'abonné: ${error.message}`);
+        next(error);
+      });
+  } else {
+    next();
+  }
 });
-} else {
-next();
-}
-});
+
 module.exports = mongoose.model("User", userSchema);
